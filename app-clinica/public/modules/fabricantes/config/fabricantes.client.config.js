6dbecd68ee80d1f5f93550294ebe3e1c0a38ad92
mlerena <manuellerena@gmail.com>
2015-02-03 14:29:11 -0200

    Remove items from menu. Start compras edit.

--- app-clinica/public/modules/fabricantes/config/fabricantes.client.config.js
+++ app-clinica/public/modules/fabricantes/config/fabricantes.client.config.js
@@ -4,8 +4,9 @@
 angular.module('fabricantes').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Fabricantes', 'fabricantes', 'dropdown', '/fabricantes(/create)?');
+		/*Menus.addMenuItem('topbar', 'Fabricantes', 'fabricantes', 'dropdown', '/fabricantes(/create)?');
 		Menus.addSubMenuItem('topbar', 'fabricantes', 'Lista de Fabricantes', 'fabricantes');
 		Menus.addSubMenuItem('topbar', 'fabricantes', 'Nuevo Fabricante', 'fabricantes/create');
+		*/
 	}
 ]);
--- app-clinica/public/modules/personas/config/personas.client.config.js
+++ app-clinica/public/modules/personas/config/personas.client.config.js
@@ -4,8 +4,10 @@
 angular.module('personas').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
+        /*
 		Menus.addMenuItem('topbar', 'Personas', 'personas', 'dropdown', '/personas(/create)?');
 		Menus.addSubMenuItem('topbar', 'personas', 'Lista de Personas', 'personas');
 		Menus.addSubMenuItem('topbar', 'personas', 'Nueva Persona', 'personas/create');
+		*/
 	}
 ]);
