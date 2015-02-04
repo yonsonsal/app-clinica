371d205f2432221e7479f0f5805447d51b6278ad
nadita <nadiadosreis@gmail.com>
2015-02-04 17:11:57 -0200

    merge hide left menu fixed

--- app-clinica/public/modules/consumos/config/consumos.client.config.js
+++ app-clinica/public/modules/consumos/config/consumos.client.config.js
@@ -5,7 +5,7 @@ angular.module('consumos').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Consumos', 'consumos', 'dropdown', '/consumos(/create)?');
-		Menus.addSubMenuItem('topbar', 'consumos', 'List Consumos', 'consumos');
-		Menus.addSubMenuItem('topbar', 'consumos', 'New Consumo', 'consumos/create');
+		Menus.addSubMenuItem('topbar', 'consumos', 'Lista de Consumos', 'consumos');
+		Menus.addSubMenuItem('topbar', 'consumos', 'Nuevo Consumo', 'consumos/create');
 	}
 ]);
--- app-clinica/public/modules/core/controllers/home.client.controller.js
+++ app-clinica/public/modules/core/controllers/home.client.controller.js
@@ -2,15 +2,92 @@
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$location',
-	function($scope, Authentication, $location) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
+    function($scope, Authentication, $location) {
+        // This provides Authentication context.
+        $scope.authentication = Authentication;
 
         if ($scope.authentication.user === ""){
-             $location.path('signin');
+            $location.path('signin');
         }
         $('.fa-bars').click();
-	}
+
+        $scope.alertList = [
+            {
+                'id': 12356,
+                'name': "producto 1",
+                'stockActual': 30,
+                'stockMinimo': 20,
+                'status': 'good',
+                'order': 3
+            },
+            {
+                'id': 434344,
+                'name': "producto 2",
+                'stockActual': 40,
+                'stockMinimo': 20,
+                'status': 'good',
+                'order': 3
+            },
+            {
+                'id': 12122,
+                'name': "producto 3",
+                'stockActual': 350,
+                'stockMinimo': 10,
+                'status': 'good',
+                'order': 3
+            }
+            ,
+            {
+                'id': 545455,
+                'name': "producto 4",
+                'stockActual': 10,
+                'stockMinimo': 10,
+                'status': 'warning',
+                'order': 2
+            },
+            {
+                'id': 53884355,
+                'name': "producto 5",
+                'stockActual': 0,
+                'stockMinimo': 10,
+                'status': 'danger',
+                'order': 1
+            },
+            {
+                'id': 44689,
+                'name': "producto 6",
+                'stockActual': 15,
+                'stockMinimo': 10,
+                'status': 'warning',
+                'order': 2
+            }
+            ,
+            {
+                'id': 564456,
+                'name': "producto 7",
+                'stockActual': 10,
+                'stockMinimo': 10,
+                'status': 'warning',
+                'order': 2
+            },
+            {
+                'id': 123158,
+                'name': "producto 8",
+                'stockActual': 0,
+                'stockMinimo': 10,
+                'status': 'danger',
+                'order': 1
+            }
+            ,
+            {
+                'id': 123158,
+                'name': "producto 9",
+                'stockActual': 30,
+                'stockMinimo': 10,
+                'status': 'good',
+                'order': 3
+            }]
+    }
 ])
 
     .directive('commonUtilities', ['$timeout', function ($timeout){
@@ -18,31 +95,29 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             restrict: 'A',
             link: function () {
 
-                function adaptContainerWidth() {
-                    if ($('#sidebar').hasClass("ng-hide")) {
-                        $('#main-content').css({
-                            'margin-left': '0px'
-                        });
-                        $('#sidebar').css({
-                            'margin-left': '-210px'
-                        });
-                        $('#sidebar > ul').hide();
-                        $("#container").addClass("sidebar-closed");
-                    } else {
-                        $('#main-content').css({
-                            'margin-left': '210px'
-                        });
-                        $('#sidebar > ul').show();
-                        $('#sidebar').css({
-                            'margin-left': '0'
-                        });
-                        $("#container").removeClass("sidebar-closed");
-                    }
-                }
-
                 $timeout(function() {
+
+                    function responsiveView() {
+                        var wSize = $(window).width();
+                        if (wSize <= 768) {
+                            $('#container').addClass('sidebar-close');
+                            $('#sidebar > ul').hide();
+                        }
+
+                        if (wSize > 768) {
+                            $('#container').removeClass('sidebar-close');
+                            $('#sidebar > ul').show();
+                        }
+                    }
+
+                    $(function() {
+                        responsiveView();
+                        $(window).on('resize', responsiveView);
+                    });
+
+
                     $('.fa-bars').click(function () {
-                        if ($('#sidebar > ul').is(":visible") === true || $('#sidebar').hasClass("ng-hide")) {
+                        if ($('#sidebar > ul').is(":visible") === true) {
                             $('#main-content').css({
                                 'margin-left': '0px'
                             });
@@ -63,23 +138,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                         }
                     });
 
-                    $(function() {
-                        function responsiveView() {
-                            var wSize = $(window).width();
-                            if (wSize <= 768) {
-                                $('#container').addClass('sidebar-close');
-                                $('#sidebar').hide();
-                            }
+                    // custom scrollbar
+                    $("#sidebar").niceScroll({styler:"fb",cursorcolor:"#4ECDC4", cursorwidth: '3', cursorborderradius: '10px', background: '#404040', spacebarenabled:false, cursorborder: ''});
 
-                            if (wSize > 768) {
-                                $('#container').removeClass('sidebar-close');
-                                $('#sidebar').show();
-                            }
-                            adaptContainerWidth ();
-                        }
-                        $(window).on('load', responsiveView);
-                        $(window).on('resize', responsiveView);
-                    });
+                    $("html").niceScroll({styler:"fb",cursorcolor:"#4ECDC4", cursorwidth: '6', cursorborderradius: '10px', background: '#404040', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
+
 
                     jQuery('#sidebar .sub-menu > a').click(function () {
                         var o = ($(this).offset());
@@ -103,12 +166,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                         });
                     });
 
-                    adaptContainerWidth ();
-
                 });
 
             }
         }
-    }])
-;
-
+    }]);
